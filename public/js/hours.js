3833cba599e876aa8f7e3bfa4aa471d2fa16499e
Kenneth Xue <kxue7438@gmail.com>
2021-01-20 21:21:12 -0500

    More

--- public/js/hours.js
+++ public/js/hours.js
@@ -1,9 +1,10 @@
         time=new Date();
         dow=time.getDay();
         hr=time.getHours();
-        if((dow==0 && hr>=9 && hr<16)||
-            (dow==6 && hr>=8 && hr<=18)||
-            (dow>0&&dow<6&&hr>=8&&hr<19)){
+        min=time.getMinutes();
+        if((dow==0 && hr>=10 && hr<=15)||
+            (dow==6 && (hr>=9&&min>=30) && hr<=18)||
+            (dow>0&&dow<6&&(hr>=9&&min>=30)&&hr<=16)){
                 $("ul#hours>#current").html("Shop is currently open!");
                 $("ul#hours>#current").css("color","green");
         }else{

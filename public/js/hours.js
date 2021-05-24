838d2106b66bfd42cc7b83a817ff37bcf84e0ffa
Kenneth Xue <kxue7438@gmail.com>
2021-05-24 04:38:39 -0400

    appointments

--- public/js/hours.js
+++ public/js/hours.js
@@ -196,7 +196,7 @@ $("#end_time, #start_time").focusout(function () {
     compare();
 });
 
-$("#end_time, #start_time, #date, #barbers").on("keyup, click", function () {
+$("#end_time, #start_time, #date, #barbers").on("keyup", function () {
     if (is_empty() == true) {
         $("#submit").prop('disabled', true);
     } else {

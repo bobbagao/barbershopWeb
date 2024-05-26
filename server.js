cb86ec310fc8cfcceaa465a581906b43158e8434
Ubuntu <ubuntu@ip-172-31-85-190.ec2.internal>
2024-05-26 06:44:49 +0000

    update title

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
-var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
+var sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+	extended: true
+}));
 app.use(bodyParser.json());
 // views is directory for all template files
 app.set('views', __dirname + '/public/views');
@@ -17,26 +17,26 @@ app.set('view engine', 'html');
 
 var validpages = '/:param(index|hours|about|contact)?';
 
-app.get('*', function(request, response) {
-  response.render('index.html');
+app.get('*', function (request, response) {
+	response.render('index.html');
 });
 
-app.post('/message',function(request,response){
+app.post('/message', function (request, response) {
 	console.log(request.toString());
 	response.end(request.toString());
 	sendgrid.send({
-	  to:       'warfieldsbarber@gmail.com',
-	  from:     request.body.email,
-	  subject:  request.body.subject,
-	  text:     'From: '+ request.body.name + ',\n' +request.body.message,
-	}, function(err, json) {
-	  if (err) { return console.error(err); }
-	  console.log(json);
+		to: 'warfieldsbarber@gmail.com',
+		from: request.body.email,
+		subject: request.body.subject,
+		text: 'From: ' + request.body.name + ',\n' + request.body.message,
+	}, function (err, json) {
+		if (err) { return console.error(err); }
+		console.log(json);
 	});
 
 	response.end("yay");
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+	console.log('Node app is running on port', app.get('port'));
 });
